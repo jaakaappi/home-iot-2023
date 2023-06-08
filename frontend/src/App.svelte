0b78ee22f338061ac8f1e0978150755980f04895
Esa Niemi <esa.niemi@protonmail.com>
2023-06-09 02:25:53 +0300

    Use real API data

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import * as _ from "lodash";
-  import Chart, { type ChartData } from "./lib/Chart.svelte";
-  import { data } from "./lib/data";
+  import { onMount } from "svelte";
+  import Chart from "./lib/Chart.svelte";
   import { formatData } from "./lib/dataUtils";
 
   const temperatureColors = [
@@ -32,14 +31,32 @@
   // #1b3cbc #115ddb #0270ee #0082ff #0091ff #1fa2ff #54b3ff #8ac8ff #b9ddff #e2f2ff
   // #008b00 #09af00 #41c300 #61d800 #75e900 #90ee02 #aaf255 #c6f68d #defabb #f2fde4
 
-  const temperatureData = formatData(data, "temperature", temperatureColors);
-  const humidityData = formatData(data, "humidity", humidityColors);
+  export let temperatureData = [];
+  export let humidityData = [];
+
+  onMount(async function () {
+    console.log(import.meta.env.VITE_API_KEY);
+
+    const response = await fetch(import.meta.env.VITE_DATA_API_URL, {
+      mode: "cors",
+      headers: {
+        authorization: import.meta.env.VITE_API_KEY,
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
+    temperatureData = formatData(data, "temperature", temperatureColors);
+    humidityData = formatData(data, "humidity", humidityColors);
+  });
 </script>
 
 <div>
   <div>
     <p>🏠Indoors</p>
-    <Chart {temperatureData} {humidityData} />
+    {#if temperatureData.length > 0}
+      <Chart {temperatureData} {humidityData} />
+    {/if}
   </div>
   <div>
     <p>☀️Outdoors</p>
