1d006efa406ae393254f8d89b404ccb78b0c4098
Esa Niemi <esa.niemi@protonmail.com>
2023-06-22 21:36:35 +0300

    Added possibility for limiting queries

--- backend/index.mjs
+++ backend/index.mjs
@@ -2,15 +2,30 @@ import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
 
 const client = new DynamoDBClient({});
 
-export const handler = async () => {
-  const command = new ScanCommand({
+export const handler = async (event) => {
+  if (
+    !Object.hasOwn(event.headers, "authorization") ||
+    event.headers.authorization !== process.env.AUTH_KEY
+  )
+    throw new Error("Bad Authorization header");
+
+  let command = new ScanCommand({
     TableName: "home-iot-2023-data",
   });
 
+  //console.log(event);
+
+  if (
+    event.hasOwnProperty("queryStringParameters") &&
+    Object.keys(event.queryStringParameters).includes("limit")
+  ) {
+    console.log(event.queryStringParameters.limit);
+    command.input.Limit = parseInt(event.queryStringParameters.limit);
+  }
+
+  //console.log(command);
   const response = await client.send(command);
   //console.log(response);
-
-  // TODO add limits
 
   const data = response.Items.flatMap((row) =>
     Object.entries(row)
@@ -26,5 +41,13 @@ export const handler = async () => {
 
   //console.log(data);
 
-  return JSON.stringify(data);
+  return {
+    statusCode: 200,
+    headers: {
+      //"Access-Control-Allow-Headers": "Content-Type,access-control-allow-headers,authorization",
+      //"Access-Control-Allow-Origin": "*",
+      //"Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
+    body: JSON.stringify(data),
+  };
 };
